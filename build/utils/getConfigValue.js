88c26b637bdfe50af41a74b5670a5b0cb4a9215d
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2020-06-18 09:14:19 -0400

    Moved to react-scripts

--- build/utils/getConfigValue.js
+++
@@ -1,5 +0,0 @@
-const isFunction = require('lodash/isFunction');
-
-const getConfigValue = (val, env = process.env.NODE_ENV) => (isFunction(val) ? val(env) : val);
-
-module.exports = getConfigValue;
